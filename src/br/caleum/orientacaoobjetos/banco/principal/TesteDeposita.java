f33fb78d13fe7bb6fbb01f40e3f25a8799b4b895
Luiz dos Reis <luizdosreis@gmail.com>
2016-11-27 10:54:24 -0200

    adicionando o Javadoc

--- src/br/caleum/orientacaoobjetos/banco/principal/TesteDeposita.java
+++ src/br/caleum/orientacaoobjetos/banco/principal/TesteDeposita.java
@@ -3,6 +3,13 @@ import br.caleum.orientacaoobjetos.banco.conta.Conta;
 import br.caleum.orientacaoobjetos.banco.conta.ContaCorrente;
 import br.caleum.orientacaoobjetos.banco.excecao.ValorInvalidoException;
 
+
+/**
+ * testa o metodo deposita da conta
+ * @author luizhenrique
+ * 
+ */
+
 public class TesteDeposita {
 	public static void main(String[] args) {
 		Conta cp = new ContaCorrente();
